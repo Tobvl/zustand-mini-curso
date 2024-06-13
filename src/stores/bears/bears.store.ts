10f96946b528b4f9074ebf539ce25d147d697b70
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-06-13 17:27:05 -0400

    se agrega persist a bears

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware';
 
 
 interface Bear {
@@ -13,9 +14,13 @@ interface BearState {
 
   bears: Bear[];
 
-  computed: {
-    totalBears: number;
-  },
+  // computed: {
+  //   totalBears: number;
+  // },
+  
+  // un FIX para utilizar la propiedad computada es que la mandemos a llamar
+  // como los otros métodos
+  totalBears: () => number;
   
   // Si quiero incrementar blackbears, aqui podemos definir
   //  como quiero que luzca la funcion que lo haga.
@@ -30,24 +35,38 @@ interface BearState {
   
 }
 
+// CONSIDERACIONES IMPORTANTES << GETTER DENTRO DE UN PERSIST >>
+// el método get() no funciona de la manera esperada cuando está
+// programado como "normalmente" estando dentro del persist
+
 // Un estado puede contar tanto con los "items(bools, objs,..)" de estado,
 // como con las funciones que modifican el estado con otra interfaz.
-export const useBearStore = create<BearState>((set, get) => ({
+export const useBearStore = create<BearState>()(
+  
+  persist(
+  (set, get) => ({
   blackBears: 10,
   polarBears: 5,
   pandaBears: 1,
 
   bears: [{id: 1, name:"Oso #1"}],
   
-  computed: {
-    // Para hacer el getter vamos a usar una propiedad
-    // de objetos de javascript (Propiedad computada)
-    get totalBears():number {
-      return get().blackBears +
-      get().polarBears +
-      get().pandaBears +
-      get().bears.length;
-    }
+  // computed: {
+  //   // Para hacer el getter vamos a usar una propiedad
+  //   // de objetos de javascript (Propiedad computada)
+  //   get totalBears():number {
+  //     return get().blackBears +
+  //     get().polarBears +
+  //     get().pandaBears +
+  //     get().bears.length;
+  //   }
+  // },
+  
+  totalBears: () => {  
+    return get().blackBears +
+    get().polarBears +
+    get().pandaBears +
+    get().bears.length;
   },
   
   increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
@@ -63,4 +82,8 @@ export const useBearStore = create<BearState>((set, get) => ({
   // clearBears: () => set(state => ({ bears: [] })), es lo mismo de abajo
   clearBears: () => set({ bears: [] }),
   
-}))
+}), 
+  {name:'bears-store'}
+)
+
+)
