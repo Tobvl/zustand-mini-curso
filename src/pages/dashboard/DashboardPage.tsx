3549ef286514babbaff2d87c1582b7391663e3db
Cristobal Sebastian Angel Sanchez Diaz <xcrisgames@gmail.com>
2024-06-16 16:32:42 -0400

    se agrega total de tareas existentes al dashboard

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,6 +1,6 @@
 import { IoAccessibilityOutline, IoHeartOutline, IoListOutline, IoLockClosedOutline, IoPawOutline } from 'react-icons/io5';
 import { WhiteCard } from '../../components';
-import { useBearStore, usePersonStore } from '../../stores';
+import { useBearStore, usePersonStore, useTaskStore } from '../../stores';
 
 export const Dashboard = () => {
 
@@ -8,6 +8,8 @@ export const Dashboard = () => {
   const totalBears = useBearStore( state => state.totalBears );
   const firstName = usePersonStore( state => state.firstName );
   const lastName = usePersonStore( state => state.lastName );
+  
+  const totalTasks = useTaskStore( state => state.totalTasks );
   
   return (
     <>
@@ -34,7 +36,7 @@ export const Dashboard = () => {
         <WhiteCard centered>
           <IoListOutline size={ 50 } className="text-indigo-600" />
           <h2>Tareas</h2>
-          <p>Información</p>
+          <p>Total de {totalTasks()}</p>
         </WhiteCard>
 
 
